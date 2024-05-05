4819d69c742a7a11be36b5b54c65eb0c50ae8dfb
Matt Wells <matt.n.wells@gmail.com>
2024-05-05 10:53:21 -0700

    Update sql_tutorial.sql

--- sql_tutorial.sql
+++ sql_tutorial.sql
@@ -578,6 +578,11 @@ ORDER BY 1;
 ---- ADVANCED SQL TUTORIAL ---- 
 -------------------------------
 
+-- Convert the funding_total_usd and founded_at_clean columns in the tutorial.crunchbase_companies_clean_date table to strings (varchar format) using a different formatting function for each one.
+
+SELECT CAST(funding_total_usd AS varchar) AS funding_total_usd_string, founded_at_clean::varchar AS founded_at_string
+FROM tutorial.crunchbase_companies_clean_date;
+
 
 
 
