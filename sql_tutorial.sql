32a1a7bdfc30412710dbf2455e163a6830e28022
Matt Wells <matt.n.wells@gmail.com>
2024-11-15 20:45:21 -0800

    Update sql_tutorial.sql

--- sql_tutorial.sql
+++ sql_tutorial.sql
@@ -666,6 +666,17 @@ SELECT sub.*
   ) sub
 WHERE sub.resolution = 'NONE';
 
+-- Write a query that displays the average number of monthly incidents for each category. Hint: use tutorial.sf_crime_incidents_cleandate to make your life a little easier.
+
+SELECT sub.category, AVG(sub.incidents) AS avg_incidents_per_month
+  FROM (
+    SELECT EXTRACT('month' FROM cleaned_date) AS month,
+      category,
+      COUNT(1) AS incidents
+    FROM tutorial.sf_crime_incidents_cleandate
+    GROUP BY 1,2
+    ) sub
+GROUP BY 1;
 
 
 
