5ab5427daf888253b6f7ecd95fa77a069f407f06
Matt Wells <matt.n.wells@gmail.com>
2025-01-09 17:43:15 -0800

    Update sql_tutorial.sql

--- sql_tutorial.sql
+++ sql_tutorial.sql
@@ -678,6 +678,21 @@ SELECT sub.category, AVG(sub.incidents) AS avg_incidents_per_month
     ) sub
 GROUP BY 1;
 
+-- Write a query that displays all rows from the three categories with the fewest incidents reported.
+
+SELECT incidents.*, sub.count AS total_incidents_in_category
+FROM tutorial.sf_crime_incidents_2014_01 incidents
+JOIN
+    (
+    SELECT category, COUNT(*) AS count
+    FROM tutorial.sf_crime_incidents_2014_01
+    GROUP BY 1
+    ORDER BY 2
+    LIMIT 3
+    ) sub
+ON sub.category = incidents.category;
+
+
 
 
 
