33ecd402d9c9936e2bb009a1867291844b90610e
Matt Wells <matt.n.wells@gmail.com>
2025-01-10 16:50:28 -0800

    Update sql_tutorial.sql

--- sql_tutorial.sql
+++ sql_tutorial.sql
@@ -710,6 +710,19 @@ LEFT JOIN (
 ON companies.quarter = acquisitions.quarter
 ORDER BY 1;
 
+-- Write a query that ranks investors from the combined dataset above by the total number of investments they have made.
+
+SELECT investor_name, COUNT(*) AS investments
+FROM (
+    SELECT *
+    FROM tutorial.crunchbase_investments_part1
+    UNION ALL
+    SELECT *
+    FROM tutorial.crunchbase_investments_part2
+    ) sub
+GROUP BY 1
+ORDER BY 2 DESC;
+
 
 
 
