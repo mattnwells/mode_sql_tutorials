94963d8489ab73b599856e1eba68c6b245ac98d6
Matt Wells <matt.n.wells@gmail.com>
2025-01-10 16:30:39 -0800

    Update sql_tutorial.sql

--- sql_tutorial.sql
+++ sql_tutorial.sql
@@ -692,6 +692,24 @@ JOIN
     ) sub
 ON sub.category = incidents.category;
 
+-- Write a query that counts the number of companies founded and acquired by quarter starting in Q1 2012. Create the aggregations in two separate queries, then join them.
+
+SELECT COALESCE(companies.quarter, acquisitions.quarter) AS quarter, companies.companies_founded, acquisitions.companies_acquired
+FROM (
+    SELECT founded_quarter AS quarter, COUNT(permalink) AS companies_founded
+    FROM tutorial.crunchbase_companies
+    WHERE founded_year >= 2012
+    GROUP BY 1
+  ) companies
+LEFT JOIN (
+    SELECT acquired_quarter AS quarter, COUNT(DISTINCT company_permalink) AS companies_acquired
+    FROM tutorial.crunchbase_acquisitions
+    WHERE acquired_year >= 2012
+    GROUP BY 1
+  ) acquisitions
+ON companies.quarter = acquisitions.quarter
+ORDER BY 1;
+
 
 
 
