07b519825081ccaa0a8aeb9794d6922a9a4786f3
Matt Wells <matt.n.wells@gmail.com>
2025-01-10 16:54:27 -0800

    Update sql_tutorial.sql

--- sql_tutorial.sql
+++ sql_tutorial.sql
@@ -723,7 +723,34 @@ FROM (
 GROUP BY 1
 ORDER BY 2 DESC;
 
+-- Write a query that does the same thing as in the previous problem, except only for companies that are still operating. Hint: operating status is in tutorial.crunchbase_companies.
 
+SELECT investor_name, COUNT(*) AS investments
+FROM (
+    SELECT *
+    FROM tutorial.crunchbase_investments_part1
+    UNION ALL
+    SELECT *
+    FROM tutorial.crunchbase_investments_part2
+    ) sub
+GROUP BY 1
+ORDER BY 2 DESC;
+
+-- Write a query that does the same thing as in the previous problem, except only for companies that are still operating. Hint: operating status is in tutorial.crunchbase_companies.
+
+SELECT investments.investor_name, COUNT(investments.*) AS investments
+FROM tutorial.crunchbase_companies companies
+JOIN (
+  SELECT *
+  FROM tutorial.crunchbase_investments_part1
+  UNION ALL
+  SELECT *
+  FROM tutorial.crunchbase_investments_part2
+  ) investments
+ON investments.company_permalink = companies.permalink
+WHERE companies.status = 'operating'
+GROUP BY 1
+ORDER BY 2 DESC;
 
 
 
