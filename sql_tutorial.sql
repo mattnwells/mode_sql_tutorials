c3e545a74347132794d074789e0595da6e9a5201
Matt Wells <matt.n.wells@gmail.com>
2025-02-21 16:11:09 -0800

    Update sql_tutorial.sql

--- sql_tutorial.sql
+++ sql_tutorial.sql
@@ -768,5 +768,26 @@ SELECT end_terminal, duration_seconds,
 FROM tutorial.dc_bikeshare_q1_2012
 WHERE start_time < '2012-01-08';
 
+-- Write a query that shows the 5 longest rides from each starting terminal, ordered by terminal, and longest to shortest rides within each terminal. Limit to rides that occurred before Jan. 8, 2012.
+
+SELECT *
+  FROM (
+        SELECT start_terminal,
+               start_time,
+               duration_seconds AS trip_time,
+               RANK() OVER (PARTITION BY start_terminal ORDER BY duration_seconds DESC) AS rank
+          FROM tutorial.dc_bikeshare_q1_2012
+         WHERE start_time < '2012-01-08'
+               ) sub
+ WHERE sub.rank <= 5;
+
+-- Write a query that shows only the duration of the trip and the percentile into which that duration falls (across the entire dataset—not partitioned by terminal).
+
+SELECT duration_seconds,
+       NTILE(100) OVER (ORDER BY duration_seconds) AS percentile
+FROM tutorial.dc_bikeshare_q1_2012
+WHERE start_time < '2012-01-08'
+ORDER BY 1 DESC;
+
 
 
